a195fd1ddde4034d8bcc0a431e4551c6e168958e
Your Name <arsalantech277@gmail.com>
2023-02-20 14:11:46 +0500

    caculator done

---
+++ MinusCalculator.mjs
@@ -0,0 +1,9 @@
+let num1 = prompt("enter your first number" );
+let num2 = prompt("enter your second number");
+
+num1 = Number(num1);
+num2 = Number(num2);
+
+let num3 = (num1 - num2 );
+
+alert("Difference is :" + num3);
